9af2ea3c52ea1f5cb32f75ac9e9cda3694342425
ThaisDuarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-03-25 12:03:21 -0300

    add - divisão

--- operações/divisão.js
+++ operações/divisão.js
@@ -3,7 +3,13 @@ function div(){
     const numero1 = document.getElementById('numero1us').value;
     const numero2 = document.getElementById('numero2us').value;
 
-    let div = Number(numero1) / Number(numero2);
+    if (numero2 = 0){
+        alert("Não é permitido dividir um número por zero!");
 
-    alert (div);
+    }else{
+        let div = Number(numero1) / Number(numero2);
+        alert (div);
+        
+    }
+
 }
