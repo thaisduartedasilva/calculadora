3c7fdabd7e00e494133506303636857513670da6
ThaisDuarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-03-25 11:48:35 -0300

    add - soma

--- operações/soma.js
+++ operações/soma.js
@@ -1,3 +1,10 @@
 function soma(){
+
+    const numero1 = document.getElementById('numero1us');
+    const numero2 = document.getElementById('numero2us');
+
+    let soma = numero1 + numero2;
+
+    alert (soma);
     
 }
